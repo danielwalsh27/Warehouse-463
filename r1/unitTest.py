494756da4aca102fdccd762cddb9aca099c5f007
AustinOblouk <austin@oblouk.com>
2019-11-18 19:53:30 -0800

    clean up after tests, add pass prints

--- r1/unitTest.py
+++ r1/unitTest.py
@@ -3,14 +3,21 @@ import main
 
 class TestStringMethods(unittest.TestCase):
     def test_1_initial_warehouses(self):
+        print("Testing Null Warehouse List")
         self.assertEqual(main.warehouses, [])
+        print("Test 1 PASS")
         
     def test_2_add_warehouse(self):
+        print("Testing Add Warehouse")
         main.warehouseWidth.text = 5
         main.warehouseHeight.text = 5
         main.onWarehouseDimensionButtonClicked()
         self.assertEqual(len(main.warehouses), 1)
         self.assertEqual(main.warehouses[0].width, 5)
+        main.warehouses = []
+        main.warehouseHeight.text = ""
+        main.warehouseWidth.text = ""
+        print("Test 2 PASS")
 
 if __name__ == '__main__':
     main.pg.display.iconify()
