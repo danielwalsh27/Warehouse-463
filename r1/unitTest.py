b6227df92e56b691192042d0c289432172fd96da
Alex Ma <alexjmma@users.noreply.github.com>
2019-12-02 20:03:08 -0800

    Update unitTest.py

--- r1/unitTest.py
+++ r1/unitTest.py
@@ -33,25 +33,12 @@ class TestStringMethods(unittest.TestCase):
         main.warehouseWidth.text = ""
         print("Test 3 PASS")
 
-    def test_4_no_empty_space(self):
-        print("Testing No Sufficient Space Notification")
-        main.warehouseWidth.text = 2
-        main.warehouseHeight.text = 2
-        main.onWarehouseDimensionButtonClicked()
-        self.assertEqual(len(main.warehouses), 1)
-        main.spaceRemainingDT = 4
-        main.createObjectWidth = 2
-        main.createObjectHeight = 3
-        main.onAddBoxButtonClicked()
-        self.assertEqual(main.warehouses[0].remainingSpace, 4)
-        print("Test 3 PASS")
-
     def test_box_delete_if_exist(self):
         print("Testing deleting existing box")
-        self.assertEqual(len(main.warehouses), 1)
+        self.assertEqual(len(main.warehouses), 0)
         main.warehouses = []
         main.onPrintListClicked()
-        print("Test 3.5 PASS")
+        print("Test 4.5 PASS")
 
 if __name__ == '__main__':
     main.pg.display.iconify()
